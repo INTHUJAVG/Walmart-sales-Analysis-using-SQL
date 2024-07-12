c1ed6ff543bc5ce0f096a1fbc3a045782b87e33f
INTHUJAVG <129651349+INTHUJAVG@users.noreply.github.com>
2024-07-12 11:03:25 +0530

    Update walmartsales.sql

--- walmartsales.sql
+++ walmartsales.sql
@@ -1,7 +1,34 @@
+#create database
+create database if not exists Walmartsales;
+# create table
+CREATE TABLE IF NOT EXISTS sales(
+	invoice_id VARCHAR(30) NOT NULL PRIMARY KEY,
+    branch VARCHAR(5) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    customer_type VARCHAR(30) NOT NULL,
+    gender VARCHAR(30) NOT NULL,
+    product_line VARCHAR(100) NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    tax_pct FLOAT(6,4) NOT NULL,
+    total DECIMAL(12, 4) NOT NULL,
+    date DATETIME NOT NULL,
+    time TIME NOT NULL,
+    payment VARCHAR(15) NOT NULL,
+    cogs DECIMAL(10,2) NOT NULL,
+    gross_margin_pct FLOAT(11,9),
+    gross_income DECIMAL(12, 4),
+    rating FLOAT(2, 1)
+);
+
+#Import the csv file 
+	
 SELECT * FROM Walmartsales.sales;
 
-# feature engineering
+# Feature engineering
+	
 select time from Walmartsales.sales;
+
 # Add the time_of_day column
 SELECT time,
 	(CASE
@@ -38,7 +65,7 @@ SET month_name=MONTHNAME(DATE);
 SELECT * FROM Walmartsales.sales;
 
 
-#GENERIC
+#GENERIC QUESTIONS
 #1.How many unique cities does the data have?
 SELECT 
 DISTINCT CITY
@@ -51,7 +78,7 @@ BRANCH
 FROM Walmartsales.sales;
 SELECT * FROM Walmartsales.sales;
 
-#PRODUCT
+#PRODUCT QUESTIONS
 #1.How many unique product lines does the data have?
 SELECT 
 count(Distinct product_line)
